161ae48f5769ce255ea1d9f07deda4bd32a161c8
Carlos Tarjano <tesserato@hotmail.com>
2025-09-07 18:01:02 +0100

    prepares to publish alpha

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use anyhow::Result;
 use clap::Parser;
-use data_plotter::cli::Cli;
+use scatters::cli::Cli;
 
 fn main() -> Result<()> {
     // Parse command-line arguments
     let cli = Cli::parse();
 
     // Run the main application logic from the library
-    if let Err(e) = data_plotter::run(&cli) {
+    if let Err(e) = scatters::run(&cli) {
         eprintln!("Error: {}", e);
         std::process::exit(1);
     }
